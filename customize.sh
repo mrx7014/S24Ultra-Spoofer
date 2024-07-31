fcec3d9f71a26e8d44c9e6bd59ba6d6c751b6037
mrx7014 <safen894@gmail.com>
2024-07-31 19:08:39 +0300

    V2.0.0

---
+++ customize.sh
@@ -0,0 +1,17 @@
+#!/system/bin/sh
+
+# Module variables
+MODPATH_SYSTEM_PROP="$MODPATH"/system.prop
+MOD_PROP_MODEL=$(grep_prop ro.product.model "$MODPATH_SYSTEM_PROP")
+MOD_PROP_PRODUCT=$(grep_prop ro.build.product "$MODPATH_SYSTEM_PROP" | tr '[:lower:]' '[:upper:]')
+MOD_PROP_VERSION=$(grep_prop ro.build.version.release "$MODPATH_SYSTEM_PROP")
+MOD_PROP_SECURITYPATCH=$(grep_prop ro.build.version.security_patch "$MODPATH_SYSTEM_PROP")
+MOD_PROP_VERSIONCODE=$(date -d "$MOD_PROP_SECURITYPATCH" '+%y%m%d')
+MOD_PROP_MONTH=$(date -d "$MOD_PROP_SECURITYPATCH" '+%B')
+MOD_PROP_YEAR=$(date -d "$MOD_PROP_SECURITYPATCH" '+%Y')
+
+# Print head message
+ui_print "- Installing, $MOD_PROP_MODEL ($MOD_PROP_PRODUCT) Prop - $MOD_PROP_MONTH $MOD_PROP_YEAR"
+
+# Print footer message
+ui_print "- Script by MRX7014, Telegram: @MRX7014"
